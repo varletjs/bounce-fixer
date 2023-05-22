7471168d77b8725813ed4bffd587a94197040d31
haoziqaq <357229046@qq.com>
2023-05-22 23:32:52 +0800

    feat: support iife and esm

---
+++ iife.js
@@ -0,0 +1,68 @@
+;(() => {
+  const inBrowser = typeof window !== 'undefined'
+  const supportTouch = inBrowser && 'ontouchstart' in window
+
+  function createBounceFixer() {
+    if (!supportTouch) {
+      return {
+        enable() {},
+        disable() {},
+      }
+    }
+
+    let startY
+
+    function handleTouchstart(event) {
+      startY = event.touches[0].clientY
+    }
+
+    function handleTouchmove(event) {
+      let el = event.target
+
+      while (el !== document.body && el !== document.documentElement) {
+        const { overflowY } = window.getComputedStyle(el)
+        const isScroller = overflowY === 'auto' || overflowY === 'scroll'
+        const allowScroll = isScroller && el.scrollHeight > el.clientHeight
+
+        if (allowScroll) {
+          const currentY = event.touches[0].clientY
+          const isReachTop = startY <= currentY && el.scrollTop == 0
+          const offsetBottom = Math.abs(
+            el.scrollHeight - el.scrollTop - el.clientHeight
+          )
+          const isReachBottom =
+            startY >= currentY && (offsetBottom === 0 || offsetBottom === 1)
+
+          if (isReachTop || isReachBottom) {
+            event.preventDefault()
+          }
+
+          return
+        }
+
+        el = el.parentNode
+      }
+    }
+
+    function enable() {
+      window.addEventListener('touchstart', handleTouchstart, {
+        passive: false,
+      })
+      window.addEventListener('touchmove', handleTouchmove, { passive: false })
+    }
+
+    function disable() {
+      window.removeEventListener('touchstart', handleTouchstart)
+      window.removeEventListener('touchmove', handleTouchmove)
+    }
+
+    return {
+      enable,
+      disable,
+    }
+  }
+
+  if (inBrowser) {
+    window.BounceFixer = { createBounceFixer }
+  }
+})()
